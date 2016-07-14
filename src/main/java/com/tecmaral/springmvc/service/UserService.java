94fcf6603d45d07cae12e0f1d0e1ab31f0298371
marcosgd <marcosgonzalezdiaz@gmail.com>
2016-07-14 14:52:10 +0200

    Spring-mvc-4-restfulervices-crud-example-resttemplate/
    pring-mvc-4-restful-web-services-crud-example-resttemplate/

---
+++ src/main/java/com/tecmaral/springmvc/service/UserService.java
@@ -0,0 +1,27 @@
+package com.tecmaral.springmvc.service;
+
+import java.util.List;
+
+import com.tecmaral.springmvc.model.User;
+
+
+
+public interface UserService {
+	
+	User findById(long id);
+	
+	User findByName(String name);
+	
+	void saveUser(User user);
+	
+	void updateUser(User user);
+	
+	void deleteUserById(long id);
+
+	List<User> findAllUsers(); 
+	
+	void deleteAllUsers();
+	
+	public boolean isUserExist(User user);
+	
+}
